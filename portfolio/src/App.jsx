9bf6e94a757eb715e668e14649ff08ca0ecb213f
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-20 10:21:10 +0530

    Header Component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,8 @@
+import Header from "./components/Header";
 export default function App(){
   return(
     <>
+    <Header />
     <h1>My Portfolio</h1>
     </>
   );
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,3 +1,14 @@
 export default function Header(){
-    
+    return(
+        <>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Certifications</li>
+            <li>Contact</li>
+        </ul>
+        </>
+
+    );
 }
