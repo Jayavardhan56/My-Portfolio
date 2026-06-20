aee94979a53765fe0de3dc8feb34f1552f59a16a
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-20 11:07:08 +0530

    Files with Initial Data

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <>
+        <h1> Hello, I am K.JAYAVARDHAN</h1>
+        <h3>CSE GRADUATE</h3>
+        </>
+    );
+}
---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,13 @@
+export default function Skills(){
+    return(
+        <>
+        <ul>
+            <li>Java</li>
+            <li>Python</li>
+            <li>AWS</li>
+            <li>Jenkins</li>
+            <li>Docker</li>
+        </ul>
+        </>
+    );
+}
