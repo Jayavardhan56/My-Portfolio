79c9b722ac0b742364c8d11ab1aa26609ca177a3
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-24 16:19:00 +0530

    Projects Testing:

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,6 +3,10 @@ export default function Projects(){
         <>
         <h1>Gluco Guard</h1>
         <p>It is a project which is useful for diabetes patients</p>
+        <h1>HealthPal+</h1>
+        <p>This is an AWS based project</p>
+        <h1>ALZDETECT</h1>
+        <p>This is a project developed for ALzheimers Patients</p>
         </>
     )
 }
