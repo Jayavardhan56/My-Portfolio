e295731a2b649338147a8041ae45bea8c3337e62
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-24 16:20:10 +0530

    Integrating Components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 export default function App(){
   return(
     <>
     <Header />
+    <Home />
+    <Projects />
+    <Skills />
     <h1>My Portfolio</h1>
     </>
   );
