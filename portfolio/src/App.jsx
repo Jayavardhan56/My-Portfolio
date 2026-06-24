468f7f988b8487646852f0b5f55fdcb285635951
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-24 16:25:15 +0530

    Home Page Mock Integration

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import About from "./components/About";
+import Certifications from "./components/Certifications";
 export default function App(){
   return(
     <>
@@ -11,6 +13,8 @@ export default function App(){
     <Projects />
     <Skills />
     <Contact />
+    <About />
+    <Certifications />
     <h1>My Portfolio</h1>
     </>
   );
