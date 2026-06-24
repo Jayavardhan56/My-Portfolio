45dcc777600dcfdbc1818e2856a7355c7a698bb2
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-24 16:17:51 +0530

    Projects Testing:

---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,8 @@
+export default function Projects(){
+    return(
+        <>
+        <h1>Gluco Guard</h1>
+        <p>It is a project which is useful for diabetes patients</p>
+        </>
+    )
+}
