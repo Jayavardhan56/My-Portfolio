728558290e3f743f338ffb0a48c20b9a13277b94
Jayavardhan <jayavardhankonathala@gmail.com>
2026-06-24 16:22:29 +0530

    Integrating Components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 export default function App(){
   return(
     <>
