1df65c67c358369838278493a5a0045a5fc13af1
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-07-04 19:35:48 +0530

    Update Certifications.jsx

--- portfolio/src/components/Certifications.jsx
+++ portfolio/src/components/Certifications.jsx
@@ -4,6 +4,7 @@ export default function Certifications(){
         <h1>Microsoft Certified Azure Fundamentals</h1>
 <h1>Azure</h1>
 This is a react application 
+Can't we comment in react
         </>
     )
 }
