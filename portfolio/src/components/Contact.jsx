fe9e8a59be65616fce9aaea92fd967eb6f7de4e3
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-07-07 17:35:28 +0530

    Update Contact.jsx

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -2,8 +2,8 @@ export default function Contact(){
     return(
         <>
         <p>jayavardhankonathala@gmail.com</p>
-Can we make this as a comment
-Contact page is under development 
+//Can we make this as a comment
+//Contact page is under development 
         </>
     )
 }
