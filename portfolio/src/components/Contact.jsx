0b9c2e45ad32a28db75a574cc90631cdf99bbf21
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-08-05 05:10:50 +0530

    Update comments in Contact component

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -3,8 +3,9 @@ export default function Contact(){
         <>
         <p>jayavardhankonathala@gmail.com</p>
         </>
-        //WHAT ALL CAN WE ADD IN CONTACTS
+        //WHAT ALL⁷ CAN WE ADD IN CONTACTS
         //Can we say confidential information in this section
         this is yes
+    Github cant ve edited on mobile
     )
 }
