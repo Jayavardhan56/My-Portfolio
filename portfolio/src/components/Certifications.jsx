f50d0904ff6f3ce6594e226d403b5a2702cfd4fc
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-08-12 10:45:09 +0530

    Update Certifications.jsx

--- portfolio/src/components/Certifications.jsx
+++ portfolio/src/components/Certifications.jsx
@@ -3,13 +3,6 @@ export default function Certifications(){
         <>
         <h1>Microsoft Certified Azure Fundamentals</h1>
 <h1>Azure</h1>
-This is a react application 
-Can't we comment in react
-React js and node js both are different 
-Both are frontend languages
-Can't we develop this
-Yeah we can in this tab space
-Cant we do it simple 
         </>
     )
 }
