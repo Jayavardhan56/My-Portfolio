fd187738f9770d028c2315082556a5aaacc92ec6
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-08-23 17:04:41 +0530

    Update About.jsx

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,7 +3,5 @@ export default function About(){
         <>
         <p>Hello I am an Graduated CSE Student</p>
         </>
-        IS BTECH WORTH
-    I AM CURRENTLY IN A CONFUSED STATE
     )
 }
