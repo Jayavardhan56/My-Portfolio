a7579b29c67ada5690f273b5b1e4da8b6b6f5dd6
Konathala Jayavardhan <155996259+Jayavardhan56@users.noreply.github.com>
2026-09-02 18:56:51 +0530

    Update Home.jsx

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -3,6 +3,7 @@ export default function Home(){
         <>
         <h1> Hello, I am K.JAYAVARDHAN</h1>
         <h3>CSE GRADUATE</h3>
+        <p>Looking for open roles in the field of Devops and IT</p>
         </>
     );
 }
